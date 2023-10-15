6e77f6526bbee7ecb650f7be6b1288f090188b1a
Corey Farrell <git@cfware.com>
2023-10-15 11:57:33 -0400

    fix!: Update development dependencies

--- debouncer.js
+++ debouncer.js
@@ -1,62 +1,62 @@
 export default class Debouncer {
-	_lastRun = 0;
-	_firstRequest = 0;
-	_lastRequest = 0;
+    _lastRun = 0;
+    _firstRequest = 0;
+    _lastRequest = 0;
 
-	constructor(callback, delay = 100, maxDelays = 2) {
-		if (typeof callback !== 'function') {
-			throw new TypeError('Function is required');
-		}
+    constructor(callback, delay = 100, maxDelays = 2) {
+        if (typeof callback !== 'function') {
+            throw new TypeError('Function is required');
+        }
 
-		this._cb = callback;
-		this._delay = delay;
-		this._maxDelays = maxDelays;
-	}
+        this._cb = callback;
+        this._delay = delay;
+        this._maxDelays = maxDelays;
+    }
 
-	_now() {
-		this._lastRun = Date.now();
-		this._cb();
-		this.clear();
-	}
+    _now() {
+        this._lastRun = Date.now();
+        this._cb();
+        this.clear();
+    }
 
-	_later() {
-		const now = Date.now();
-		const delta = now - this._lastRequest;
-		const totalDelta = now - this._firstRequest;
-		const maxDelay = this._delay * this._maxDelays;
-		const maxTimeout = Math.min(maxDelay - totalDelta, this._delay - delta);
+    _later() {
+        const now = Date.now();
+        const delta = now - this._lastRequest;
+        const totalDelta = now - this._firstRequest;
+        const maxDelay = this._delay * this._maxDelays;
+        const maxTimeout = Math.min(maxDelay - totalDelta, this._delay - delta);
 
-		if (this._lastRun && maxTimeout > 0) {
-			this._timeout = setTimeout(() => this._later(), maxTimeout);
-		} else {
-			this._now();
-		}
-	}
+        if (this._lastRun && maxTimeout > 0) {
+            this._timeout = setTimeout(() => this._later(), maxTimeout);
+        } else {
+            this._now();
+        }
+    }
 
-	run(immediately = false) {
-		const now = Date.now();
+    run(immediately = false) {
+        const now = Date.now();
 
-		this._lastRequest = now;
-		if (!this._firstRequest) {
-			this._firstRequest = now;
-		}
+        this._lastRequest = now;
+        if (!this._firstRequest) {
+            this._firstRequest = now;
+        }
 
-		if (immediately) {
-			this._now();
-		} else if (!this._timeout) {
-			this._timeout = setTimeout(() => this._later(), this._delay);
-		}
-	}
+        if (immediately) {
+            this._now();
+        } else if (!this._timeout) {
+            this._timeout = setTimeout(() => this._later(), this._delay);
+        }
+    }
 
-	flush() {
-		if (this._timeout) {
-			this._now();
-		}
-	}
+    flush() {
+        if (this._timeout) {
+            this._now();
+        }
+    }
 
-	clear() {
-		this._firstRequest = 0;
-		this._lastRequest = 0;
-		this._timeout = clearTimeout(this._timeout);
-	}
+    clear() {
+        this._firstRequest = 0;
+        this._lastRequest = 0;
+        this._timeout = clearTimeout(this._timeout);
+    }
 }
--- test.js
+++ test.js
@@ -1,163 +1,161 @@
-import {promisify} from 'node:util';
+import {setTimeout as delay} from 'node:timers/promises';
 import t from 'libtap';
 
 import Debouncer from '@cfware/debouncer';
 
-const delay = promisify(setTimeout);
-
 class TestObject {
-	constructor(t, ...args) {
-		this.runs = 0;
-		this.start = Date.now() + 2;
-		this.t = t;
-		this.d = new Debouncer(() => {
-			this.runs++;
-		}, ...args);
-	}
+    constructor(t, ...args) {
+        this.runs = 0;
+        this.start = Date.now() + 2;
+        this.t = t;
+        this.d = new Debouncer(() => {
+            this.runs++;
+        }, ...args);
+    }
 
-	run(immediately) {
-		this.d.run(immediately);
-	}
+    run(immediately) {
+        this.d.run(immediately);
+    }
 
-	flush() {
-		this.d.flush();
-	}
+    flush() {
+        this.d.flush();
+    }
 
-	clear() {
-		this.d.clear();
-	}
+    clear() {
+        this.d.clear();
+    }
 
-	async checkAfter(ms, expected) {
-		await delay(ms);
+    async checkAfter(ms, expected) {
+        await delay(ms);
 
-		this.t.equal(this.runs, expected, `after ${ms}ms delay`);
-	}
+        this.t.equal(this.runs, expected, `after ${ms}ms delay`);
+    }
 }
 
 t.test('exports constructor', async t => {
-	t.type(Debouncer, 'function');
-	t.throws(() => new Debouncer(), 'Function is required');
-	t.type(new Debouncer(() => {}), Debouncer);
+    t.type(Debouncer, 'function');
+    t.throws(() => new Debouncer(), 'Function is required');
+    t.type(new Debouncer(() => {}), Debouncer);
 });
 
 t.test('single call to .run', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.run();
+    d.run();
 
-	return Promise.all([
-		d.checkAfter(75, 0),
-		d.checkAfter(125, 1),
-		d.checkAfter(250, 1)
-	]);
+    return Promise.all([
+        d.checkAfter(75, 0),
+        d.checkAfter(125, 1),
+        d.checkAfter(250, 1)
+    ]);
 });
 
 t.test('two calls to .run within first delay', async t => {
-	const d = new TestObject(t, 100);
+    const d = new TestObject(t, 100);
 
-	d.run();
-	setTimeout(() => d.run(), 5);
+    d.run();
+    setTimeout(() => d.run(), 5);
 
-	return Promise.all([
-		d.checkAfter(75, 0),
-		d.checkAfter(125, 1),
-		d.checkAfter(250, 1)
-	]);
+    return Promise.all([
+        d.checkAfter(75, 0),
+        d.checkAfter(125, 1),
+        d.checkAfter(250, 1)
+    ]);
 });
 
 t.test('two calls after first run', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.run();
-	setTimeout(() => d.run(), 110);
-	setTimeout(() => d.run(), 150);
+    d.run();
+    setTimeout(() => d.run(), 110);
+    setTimeout(() => d.run(), 150);
 
-	return Promise.all([
-		d.checkAfter(75, 0),
-		d.checkAfter(125, 1),
-		d.checkAfter(225, 1),
-		d.checkAfter(275, 2),
-		d.checkAfter(375, 2)
-	]);
+    return Promise.all([
+        d.checkAfter(75, 0),
+        d.checkAfter(125, 1),
+        d.checkAfter(225, 1),
+        d.checkAfter(275, 2),
+        d.checkAfter(375, 2)
+    ]);
 });
 
 t.test('flush without run', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.flush();
+    d.flush();
 
-	return d.checkAfter(150, 0);
+    return d.checkAfter(150, 0);
 });
 
 t.test('flush after run', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.run();
-	setTimeout(() => d.flush(), 50);
+    d.run();
+    setTimeout(() => d.flush(), 50);
 
-	return Promise.all([
-		d.checkAfter(25, 0),
-		d.checkAfter(75, 1),
-		d.checkAfter(250, 1)
-	]);
+    return Promise.all([
+        d.checkAfter(25, 0),
+        d.checkAfter(75, 1),
+        d.checkAfter(250, 1)
+    ]);
 });
 
 t.test('immediate flush', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.run(true);
+    d.run(true);
 
-	return Promise.all([
-		d.checkAfter(0, 1),
-		d.checkAfter(250, 1)
-	]);
+    return Promise.all([
+        d.checkAfter(0, 1),
+        d.checkAfter(250, 1)
+    ]);
 });
 
 t.test('clear without run', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.clear();
+    d.clear();
 
-	return d.checkAfter(150, 0);
+    return d.checkAfter(150, 0);
 });
 
 t.test('clear during delay', async t => {
-	const d = new TestObject(t);
+    const d = new TestObject(t);
 
-	d.run();
-	setTimeout(() => d.clear(), 50);
+    d.run();
+    setTimeout(() => d.clear(), 50);
 
-	return d.checkAfter(175, 0);
+    return d.checkAfter(175, 0);
 });
 
 t.test('maximum delays', async t => {
-	// 100ms minimum delay, 200ms maximum.
-	const d = new TestObject(t, 100, 2);
+    // 100ms minimum delay, 200ms maximum.
+    const d = new TestObject(t, 100, 2);
 
-	d.run();
-	d.flush();
+    d.run();
+    d.flush();
 
-	// Initial schedule is for +100ms
-	d.run();
+    // Initial schedule is for +100ms
+    d.run();
 
-	// This will defer to 160ms
-	setTimeout(() => d.run(), 60);
-	// This will defer to 200ms
-	setTimeout(() => d.run(), 140);
-	// The will not defer, already scheduled at 200ms / maximum delay.
-	setTimeout(() => d.run(), 180);
+    // This will defer to 160ms
+    setTimeout(() => d.run(), 60);
+    // This will defer to 200ms
+    setTimeout(() => d.run(), 140);
+    // The will not defer, already scheduled at 200ms / maximum delay.
+    setTimeout(() => d.run(), 180);
 
-	// This schedules for 320ms
-	setTimeout(() => d.run(), 220);
+    // This schedules for 320ms
+    setTimeout(() => d.run(), 220);
 
-	// This will defer to 380ms
-	setTimeout(() => d.run(), 280);
+    // This will defer to 380ms
+    setTimeout(() => d.run(), 280);
 
-	return Promise.all([
-		d.checkAfter(180, 1),
-		d.checkAfter(220, 2),
-		d.checkAfter(360, 2),
-		d.checkAfter(400, 3),
-		d.checkAfter(500, 3)
-	]);
+    return Promise.all([
+        d.checkAfter(180, 1),
+        d.checkAfter(220, 2),
+        d.checkAfter(360, 2),
+        d.checkAfter(400, 3),
+        d.checkAfter(500, 3)
+    ]);
 });
